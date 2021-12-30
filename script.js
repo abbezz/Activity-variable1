f3cc539b2a358e003edcd2ae24b54c9529f3ee8c
Abdollah Zamannoun <abbezz@live.com>
2021-12-30 21:19:19 +0000

    Added starter code

---
+++ script.js
@@ -0,0 +1,22 @@
+// declare variables
+
+var personName = "Bob";
+var pets = 2;
+var funFact = 'I am from Mars';
+
+// display messages
+
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' per(s).');
+console.log('Fun fact: ' + funFact + '.');
+
+// reassign variables
+personName = 'Alice';
+pets = 4;
+funFact = 'I am from Earth';
+
+//display messages
+
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' per(s).');
+console.log('Fun fact: ' + funFact + '.');
